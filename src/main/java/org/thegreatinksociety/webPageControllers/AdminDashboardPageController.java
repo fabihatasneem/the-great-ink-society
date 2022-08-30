eb25a7f635dfe80948d2bee3e29afac7f41f690f
iftekharzeeon <1805038@ugrad.cse.buet.ac.bd>
2022-08-30 19:46:02 +0600

    Admin Page started

--- src/main/java/org/thegreatinksociety/webPageControllers/AdminDashboardPageController.java
+++ src/main/java/org/thegreatinksociety/webPageControllers/AdminDashboardPageController.java
@@ -1,4 +1,19 @@
 package org.thegreatinksociety.webPageControllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thegreatinksociety.global.GlobalFunction;
+import org.thegreatinksociety.global.GlobalVariable;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class AdminDashboardPageController {
+
+    @RequestMapping("/admin/dashBoard")
+    public String getBookCreatePage(HttpSession session, Model model) {
+
+        return "/admin/adminDashboard";
+    }
 }
--- src/main/java/org/thegreatinksociety/webPageControllers/AdminLoginPageController.java
+++ src/main/java/org/thegreatinksociety/webPageControllers/AdminLoginPageController.java
@@ -1,4 +1,19 @@
 package org.thegreatinksociety.webPageControllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thegreatinksociety.global.GlobalFunction;
+import org.thegreatinksociety.global.GlobalVariable;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class AdminLoginPageController {
+
+    @RequestMapping("/admin/login")
+    public String getAdminLoginPage(HttpSession session, Model model) {
+
+        return "/admin/adminLogin";
+    }
 }
